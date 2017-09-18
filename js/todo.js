f1127e8ace5902af082619820b9133f598d54baa
andrew-rayco <andy@andycondon.com>
2017-09-18 13:13:56 +1200

    long time no commit

--- js/todo.js
+++ js/todo.js
@@ -9,61 +9,24 @@ $(function() {
   var $errorMsg = $($.parseHTML(emptyItemText));
   var $listItems = $('li');
   var $listItemText = $('li a').text;
+  var todosArray = []
   var database = firebase.database();
 
-  console.log(database)
-
-  function addItemToDb() {
-    // var newPostKey = database.ref().child('list').push().key;
-    // database.ref('list' + newPostKey).set({
-    //   item: 'Shoe shine from firebase'
-    // })
-
-    // database.ref('list-KuDgnRz_PqLHBS9lSDC').remove()
-  }
-  addItemToDb()
-
   database.ref('list').on('value', function(snapshot) {
-    console.log(snapshot.val())
+    // snapshot.val() gives the current state of the db
+    displayItems(snapshot.val())
   })
 
-  // Check for existing JSON list data
-  // var xhr = new XMLHttpRequest();
-  //
-  // xhr.onload = function() {
-  //   if (xhr.status === 200) {
-  //
-  //     // parse JSON object into JavaScript object
-  //     responseObject = JSON.parse(xhr.responseText);
-  //
-  //     // loop through list items and add to list
-  //     for (var i=0; i < responseObject.list.length; i++) {
-  //
-  //       // If list is currently empty, add JSON items immediately
-  //       if ($listItems.length < 1) {
-  //         $('ul').append('<li class="todo-item"><a href="#">x</a> ' + responseObject.list[i].item + '</li>');
-  //       } else {
-  //         for (var j=0; j < $listItems.length; j++) {
-  //           // Get list item string and remove the first character (the close x)
-  //           var $listItemNoX = $listItems.eq(j).text().slice(1);
-  //           if (responseObject.list[i].item !== $listItemNoX) {
-  //             $('ul').append('<li class="todo-item"><a href="#">x</a> ' + responseObject.list[i].item + '</li>');
-  //           } else {
-  //             $form.prepend('<p class="error">Item already exists</p>');
-  //             $('form p').delay(1000).fadeOut(300);
-  //           }
-  //         }
-  //       }
-  //     }
-  //   } else {
-  //     // Show error message and code if XMLHttpRequest fails
-  //     var $jsonFailMsg = '<p class="error">Failed to find list. Error ' + xhr.status + '</p>';
-  //     $form.prepend($jsonFailMsg);
-  //   }
-  // };
-  //
-  // xhr.open('GET', 'json/list.json', 'true');
-  // xhr.send(null);
+  function displayItems(allTodos) {
+    // clear the ul of li's
+    $('ul').empty()
+
+    console.log(allTodos)
+    for (var key in allTodos) {
+      $('ul').append('<li class="todo-item"><a id="' + allTodos[key].id + '" href="#">x</a> ' + allTodos[key].item + '</li>');
+    }
+    console.log(todosArray)
+  }
 
   function addItem() {
     // Add new item to list
@@ -73,29 +36,26 @@ $(function() {
 
         // Add item to list if not empty string
         var newText = $textInput.val();
+        todosArray.push(newText)
         // $('ul').append('<li class="todo-item"><a href="#">x</a> ' + newText + '</li>');
         $textInput.val('');
 
+
         function addItemToDb(newListItem) {
-          // A post entry.
-          var postData = {
-            item: newListItem
-          };
 
           // Get a key for a new Post.
           var newPostKey = firebase.database().ref().child('posts').push().key;
+          var singleItem = {
+            item: newListItem,
+            id: newPostKey,
+            order: todosArray.length
+          };
+          console.log(newPostKey)
 
-          database.ref('list' + newPostKey).set(postData)
+          database.ref('list/' + newPostKey).set(singleItem)
         }
 
         addItemToDb(newText)
-
-        // Add item to JSON file ================== Trying to...
-        // var listLength = responseObject;
-        // console.log($listItemText);
-        // responseObject.list[1].item = newText;
-        // xhr.open('POST', 'json/list.json', 'true');
-        // xhr.send(null);
 
       } else {
         var $newMsg = $($errorMsg).hide().fadeIn(1000);
@@ -116,6 +76,8 @@ $(function() {
     $('ul').on('click', '.todo-item>a', function(e) {
       e.preventDefault();
       $(this).parent().fadeOut(400);
+      database.ref('list/' + e.target.id).remove()
+      console.log(e.target.id)
     });
 
   };
@@ -123,7 +85,18 @@ $(function() {
 
   // Sort items using jQuery UI
   $(function() {
-    $("ul").sortable();
+    var sortEventHandler = function(event, ui) {
+      var arrayToBeSorted = $('.todo-item')
+      var listValues = []
+      console.log(arrayToBeSorted[0])
+
+      for (var i=0; i<arrayToBeSorted.length; i++) {
+        listValues.push(arrayToBeSorted[i].innerHTML)
+      }
+    }
+    $("ul").sortable({
+      stop: sortEventHandler
+    });
     $("ul").disableSelection();
   });
 
